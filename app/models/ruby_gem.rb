2998783455d29cfc0e2a8ed14a12adf4fafcdfa7
Byron Appelt <byron.appelt@gmail.com>
2014-11-08 16:32:13 -0600

    remove retry logic since failed jobs are saved and can be retried in resque

--- app/models/ruby_gem.rb
+++ app/models/ruby_gem.rb
@@ -78,38 +78,26 @@ class RubyGem
   end
 
   def self.pull_spec_and_create(gem_name)
-    retries = 3
-    begin
-      new_gem = RubyGem.find_by(name: gem_name)
-      new_gem = RubyGem.create!(name: gem_name) unless new_gem.present?
-      puts "processing gem  #{gem_name}"
+    new_gem = RubyGem.find_by(name: gem_name)
+    new_gem = RubyGem.create!(name: gem_name) unless new_gem.present?
+    puts "processing gem  #{gem_name}"
 
-      gem_url = "https://rubygems.org/api/v1/gems/#{gem_name}.json"
+    gem_url = "https://rubygems.org/api/v1/gems/#{gem_name}.json"
 
-      gem_spec_str = RestClient.get(gem_url)
+    gem_spec_str = RestClient.get(gem_url)
 
-      gem_spec = JSON.parse(gem_spec_str)
-      dependencies = gem_spec['dependencies']['runtime']
-      dependency_gem_names = dependencies.collect { |gem| gem['name'] }
-      dependency_list = []
-      dependency_gem_names.each do |dependency_name|
-        puts "|--- finding dependency #{dependency_name}"
-        dependency = RubyGem.find_by(name: dependency_name)
-        dependency = RubyGem.create!(name: dependency_name) if dependency.nil?
-        dependency_list << dependency
-      end
-      new_gem.dependencies = dependency_list
-      new_gem.save!
-    rescue StandardError => e
-      puts $!
-      retries -= 1
-      if retries > 0
-        puts "retrying #{gem_name}"
-        retry
-      else
-        raise e
-      end
+    gem_spec = JSON.parse(gem_spec_str)
+    dependencies = gem_spec['dependencies']['runtime']
+    dependency_gem_names = dependencies.collect { |gem| gem['name'] }
+    dependency_list = []
+    dependency_gem_names.each do |dependency_name|
+      puts "|--- finding dependency #{dependency_name}"
+      dependency = RubyGem.find_by(name: dependency_name)
+      dependency = RubyGem.create!(name: dependency_name) if dependency.nil?
+      dependency_list << dependency
     end
+    new_gem.dependencies = dependency_list
+    new_gem.save!
   end
 
 end

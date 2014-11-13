7fc22c0b5c342413f0501f43cf00c55bbb020957
Byron Appelt <byron.appelt@gmail.com>
2014-11-12 19:03:36 -0600

    can't use search API since results are limited to 1,000 follow pagination

--- lib/git_hub_client.rb
+++ lib/git_hub_client.rb
@@ -5,14 +5,24 @@ class GitHubClient
   end
 
   def process_ruby_repos
-    @client.search_repositories('language:ruby').items.each do |repo|
+    result = @client.all_repositories
+    result.each do |repo|
       yield repo[:full_name]
+    end
+    last_response = @client.last_response
+    while last_response.rels[:next]
+      puts "getting #{last_response.rels[:next].href}"
+      last_response = last_response.rels[:next].get
+      last_response.data.each do |repo|
+        yield repo[:full_name]
+      end
     end
     true
   end
 
+
   def get_gemfile(full_repo_name)
-    result = @client.contents(full_repo_name,path: 'Gemfile')[:content]
+    result = @client.contents(full_repo_name, path: 'Gemfile')[:content]
     Base64.decode64 result
   rescue Octokit::NotFound
     return ''

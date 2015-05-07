d1113d045af0402539935ce785e3c929f9b81fe8
Byron Appelt <byron.appelt@gmail.com>
2015-05-07 09:47:46 -0500

    fix rendering of scores for unranked gems

--- app/helpers/ruby_gems_helper.rb
+++ app/helpers/ruby_gems_helper.rb
@@ -1,7 +1,11 @@
 module RubyGemsHelper
   def format_score(score)
     if score
-      number_with_precision(score, delimiter: ',', precision: 0)
+      %Q[
+          <strong class="score" data-tooltip title="As of: #{@ruby_gem.ranked_at.to_s(:short)}">
+            #{number_with_precision(score, delimiter: ',', precision: 0)}
+          </strong>
+        ].html_safe
     else
       '?'
     end
